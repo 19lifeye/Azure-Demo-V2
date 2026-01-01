ccf38a608161bba33b7b34fad27b0b54325b276a
Pratik <pratiktikarye007@gmail.com>
2026-01-01 10:43:17 +0530

    second commit

--- src/main/java/com/azuredemo/deployment/DeploymentApplication.java
+++ src/main/java/com/azuredemo/deployment/DeploymentApplication.java
@@ -12,7 +12,7 @@ public class DeploymentApplication {
 	
 	@GetMapping("/message")
 	public String message(){
-		return "Congrats ! your application deployed successfully in Azure Platform. !";
+		return "Congrats ! your application deployed successfully in Azure Platform. 2nd trial!";
 	}
 	
 	public static void main(String[] args) {
